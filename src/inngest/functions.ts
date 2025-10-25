ccf27a935ddfc8480adac6cfaa7df534e9afd6bd
taxilpatel132 <taxilmovaliya123@gmail.com>
2025-10-25 10:23:26 +0530

    error-tracking

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -3,17 +3,25 @@ import { inngest } from "./client";
 import { createGoogleGenerativeAI } from "@ai-sdk/google";
 import { generateText } from "ai";
 const google = createGoogleGenerativeAI();
+import * as Sentry from "@sentry/nextjs";
 
 export const executeAi = inngest.createFunction(
   { id: "execute-ai" },
   { event: "execute/ai" },
   async ({ event, step }) => {
+  
 
+  Sentry.logger.info('User triggered test log', { log_source: 'sentry_test' })
     await step.sleep('pretend','5s');
     const steps=await step.ai.wrap("gemini-generate-text", generateText,{
       model: google("gemini-2.5-flash"),
       system: "You are a helpful assistant.",
       prompt: "what is 2+2?",
+       experimental_telemetry: {
+    isEnabled: true,
+    recordInputs: true,
+    recordOutputs: true,
+  },
     });
 
     return steps;
